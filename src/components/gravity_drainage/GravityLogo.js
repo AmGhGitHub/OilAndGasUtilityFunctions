228aeec1c9de0965662010e6711a59aea5219a9d
Amir Ghaderi <altghaderi@gmail.com>
2022-01-31 16:26:44 -0700

    made the logo image responsive

--- src/components/gravity_drainage/GravityLogo.js
+++ src/components/gravity_drainage/GravityLogo.js
@@ -3,7 +3,7 @@ const logo = require("../../images/GD_Logo3.jpg");
 const GravityLogo = () => {
   return (
     <section className="d-flex flex-row justify-content-center mt-3">
-      <img src={logo} alt="shap logo" height="150px" />
+      <img src={logo} alt="shap logo" class="img-fluid" />
     </section>
   );
 };
